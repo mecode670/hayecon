bfaf0612776a558cefb74e0fb26fc03192ebe05c
ibraim <mecode670@gmail.com>
2021-12-31 08:02:39 -0500

    Changed UI

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState, useContext } from 'react';
 import Sidebar from './Sidebar';
 import contextCreate from '../context/contextCreate.js';
 import Dropdown from './Dropdown';
+import { MenuAlt3Icon, XIcon } from '@heroicons/react/solid';
 
 const Header = () => {
   const context = useContext(contextCreate);
@@ -52,59 +53,11 @@ const Header = () => {
               aria-label='Navigation bar full screen'
             >
               {isShow ? (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-6 w-6'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth={2}
-                    d='M6 18L18 6M6 6l12 12'
-                  />
-                </svg>
+                <XIcon className={'h-5 w-5'} />
               ) : (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-6 w-6'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth={2}
-                    d='M4 8h16M4 16h16'
-                  />
-                </svg>
+                <MenuAlt3Icon className={'h-5 w-5'} />
               )}
             </button>
-            {/* <button
-              onClick={onClickn25f32d}
-              className={`${
-                windowWidth > 500 ? 'visible' : 'hidden'
-              } flex mt-4`}
-              aria-label='Navigation bar full screen'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M19 9l-7 7-7-7'
-                />
-              </svg>
-            </button> */}
           </div>
         </nav>
       </header>
--- pages/continue/index.jsx
+++ pages/continue/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { useState } from 'react';
-import { Switch } from '@headlessui/react';
 import Link from 'next/link';
+import { MailIcon, PhoneIcon } from '@heroicons/react/solid';
 
 const Continue = () => {
   const [toggle, setToggle] = useState(false);
@@ -34,7 +34,7 @@ const Continue = () => {
               </h2>
               <p className='text-xl text-gray-600 text-center'>Welcome back!</p>
               <Link href='#'>
-                <a className='flex items-center justify-center mt-4 hover:text-paper text-white transition duration-500 ease-in-out hover:bg-white rounded-2xl shadow-lg bg-paper'>
+                <a className='flex items-center justify-center mt-4 hover:text-white text-paper transition duration-500 ease-in-out hover:bg-paper rounded-2xl shadow-lg bg-white'>
                   <div className='px-4 py-3 hover:text-paper'>
                     <svg className='h-6 w-6' viewBox='0 0 40 40'>
                       <path
@@ -55,63 +55,79 @@ const Continue = () => {
                       />
                     </svg>
                   </div>
-                  <h1 className='px-4 py-3 w-5/6 text-center font-readex'>
+                  <h1 className=' select-none px-4 py-3 w-5/6 text-center font-readex'>
                     Continue with Google
                   </h1>
                 </a>
               </Link>
-              <div className='mt-4 flex items-center justify-between'>
-                <span className='border-b w-1/5 lg:w-1/4' />
-                <Link href='#'>
-                  <a className='text-xs text-center text-gray-500 uppercase'>
-                    or Continue with email
+              <Link href='#'>
+                <a className='flex items-center justify-center mt-4 hover:text-white text-paper transition duration-500 ease-in-out hover:bg-paper rounded-2xl shadow-lg bg-white'>
+                  <div className='px-4 py-3 hover:text-paper'>
+                    <svg className='h-6 w-6' viewBox='0 0 40 40'>
+                      <path
+                        d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
+                        fill='#FFC107'
+                      />
+                      <path
+                        d='M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z'
+                        fill='#FF3D00'
+                      />
+                      <path
+                        d='M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z'
+                        fill='#4CAF50'
+                      />
+                      <path
+                        d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
+                        fill='#1976D2'
+                      />
+                    </svg>
+                  </div>
+                  <h1 className=' select-none px-4 py-3 w-5/6 text-center font-readex'>
+                    Continue with Google
+                  </h1>
+                </a>
+              </Link>
+              <Link href='#'>
+                <a className='flex items-center justify-center mt-4 hover:text-white text-paper transition duration-500 ease-in-out hover:bg-paper rounded-2xl shadow-lg bg-white'>
+                  <div className='px-4 py-3 hover:text-paper'>
+                    <PhoneIcon className={'h-6 w-6'} />
+                  </div>
+                  <h1 className=' select-none px-4 py-3 w-5/6 text-center font-readex'>
+                    Continue with Phone
+                  </h1>
+                </a>
+              </Link>
+              <div className='mt-4 select-none '>
+                <div className='mt-6 mb-2 flex items-center justify-between'>
+                  <span className='border-b w-1/5 lg:w-1/4 ' />
+                  <a
+                    href='#email'
+                    className='text-xs select-none text-center text-gray-500 uppercase'
+                  >
+                    or Continue with Email
                   </a>
-                </Link>
-                <span className='border-b w-1/5 lg:w-1/4' />
-              </div>
-              <div className='mt-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2'>
-                  Email Address
-                </label>
-                <input
-                  className='bg-white text-paper focus:outline-none focus:shadow-outline border border-paper rounded-2xl py-2 px-4 block w-full appearance-none'
-                  type='email'
-                  aria-label='input email'
-                />
-              </div>
-              <div className='mt-4'>
+                  <span className='border-b w-1/5 lg:w-1/4' />
+                </div>
                 <div className='flex justify-between'>
-                  <label className='block text-gray-700 text-sm font-bold mb-2'>
-                    Password
+                  <label
+                    id='email'
+                    className=' flex select-none text-gray-700 text-sm font-bold mb-2'
+                  >
+                    <MailIcon className={'h-6 w-6 mr-2'} /> Email
                   </label>
-                  <Link href='#'>
-                    <a className='text-xs text-gray-500 hover:text-paper'>
-                      Forget Password?
-                    </a>
-                  </Link>
                 </div>
                 <input
                   className='bg-white mb-4 text-paper focus:outline-none focus:shadow-outline border border-paper rounded-2xl py-2 px-4 block w-full appearance-none z-10'
-                  type='password'
-                  aria-label='Input Password'
+                  type='email'
+                  aria-label='Input Email'
                 />
-                <Switch
-                  checked={toggle}
-                  onChange={setToggle}
-                  className='relative transition duration-500 ease-in-out flex items-center h-6 lg:left-80 left-64 rounded-full z-20 bottom-12 w-11 bg-paper '
-                >
-                  <span className='sr-only'>Show Password</span>
-                  <span
-                    className={`${
-                      toggle ? 'translate-x-6' : 'translate-x-1'
-                    } inline-block w-4 h-4 transform bg-white rounded-full`}
-                  />
-                </Switch>
               </div>
-              <div className='mt-8'>
-                <button className='bg-paper text-white shadow-lg hover:bg-white transition duration-500 ease-in-out  font-readex py-2 px-4 w-full rounded-full hover:text-paper'>
-                  Continue
-                </button>
+              <div className='mt-4  select-none '>
+                <div className='mt-8'>
+                  <button className='bg-paper text-white shadow-lg hover:bg-white transition duration-500 ease-in-out  font-readex py-2 px-4 w-full rounded-full hover:text-paper  select-none '>
+                    Continue
+                  </button>
+                </div>
               </div>
             </div>
           </div>

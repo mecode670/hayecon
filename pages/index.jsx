d6eda0fc6c800d37e0421501e3c9419950a6789f
ibraim <mecode670@gmail.com>
2021-12-31 01:59:39 -0500

    Commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import paepr_big from '../public/paepr-big.png';
 import Link from 'next/link';
 export default function Home() {
   return (

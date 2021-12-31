7925549694ceda64dda220735d28a05985c841d9
ibraim <mecode670@gmail.com>
2021-12-31 02:48:32 -0500

    Applied Different svg

--- components/Header.jsx
+++ components/Header.jsx
@@ -61,6 +61,27 @@ const Header = () => {
                 />
               </svg>
             </button>
+            <button
+              className={`${
+                windowWidth > 500 ? 'visible' : 'hidden'
+              } flex mt-4`}
+              aria-label='Navigation bar full screen'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M19 9l-7 7-7-7'
+                />
+              </svg>
+            </button>
           </div>
         </nav>
       </header>
